e2c8e3e34af203a14c07a7c8f62e8607f119a321
ismail <ismail@arkulpa.at>
2025-01-16 16:03:38 +0100

    in progress

---
+++ icons/ChevronIcon.tsx
@@ -0,0 +1,18 @@
+export const ChevronIcon = (props: { className?: string }) => {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5"
+      />
+    </svg>
+  );
+};
