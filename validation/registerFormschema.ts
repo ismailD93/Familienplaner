64cbbb02572d2a5ccdd066fbd55a9f1b874c1e54
Umit2711 <uemit.tamyol@gmail.com>
2025-02-07 08:16:43 +0100

    logo and confirm password added

--- validation/registerFormschema.ts
+++ validation/registerFormschema.ts
@@ -5,6 +5,8 @@ const getRegisterFormSchema = () =>
     username: Yup.string().required("erforderlich"),
     email: Yup.string().email().required("erforderlich"),
     password: Yup.string().min(1, "").required("erforderlich"),
+    confirmPassword: Yup.string().oneOf([Yup.ref("password"), ""], "Passwörter stimmen nicht überein.")
+    .required("erforderlich"),
     });
 
 export default getRegisterFormSchema;
